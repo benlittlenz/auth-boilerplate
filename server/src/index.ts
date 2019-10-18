0930f1513f879f49b0fd8e971d169b19824ed8f2
nzbenji <benlittle53@gmail.com>
2019-10-18 09:53:51 +0100

    can now access token on successful signin

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,6 +7,8 @@ import { ApolloServer } from "apollo-server-express";
 import { createConnection } from "typeorm";
 import cookieParser from "cookie-parser";
 import { verify } from "jsonwebtoken";
+import { User } from "./entity/User";
+import { createAccessToken } from "./auth";
 
 (async () => {
   const app = express();
@@ -15,8 +17,23 @@ import { verify } from "jsonwebtoken";
     res.send("hidffs");
   });
 
-  app.post("/refresh_token", (req, res) => {
-    console.log(req.headers)
+  app.post("/refresh_token", async (req, res) => {
+    const token = req.cookies.gdsfs;
+    if (!token) return res.send({ ok: false, accessToken: "" });
+    let payload: any = null;
+    try {
+      payload = verify(token, process.env.REFRESH_TOKEN_SECRET!);
+    } catch (err) {
+      console.log(err);
+      return res.send({ ok: false, accessToken: "" });
+    }
+
+    //if payload is valid, send back an access token
+    const user = await User.findOne({ id: payload.userId });
+    if(!user) {
+        return res.send({ ok: false, accessToken: "" });
+    }
+    return res.send({ ok: true, accessToken: createAccessToken(user) });
   });
 
   await createConnection();
