2d3b0a3f7ae7ed17bdb45c75204a44119b461252
nzbenji <benlittle53@gmail.com>
2019-10-18 08:00:28 +0100

    login mutation

--- server/src/UserResolver.ts
+++ server/src/UserResolver.ts
@@ -1,13 +1,51 @@
-import { Resolver, Query, Mutation, Arg } from "type-graphql";
+import {
+  Resolver,
+  Query,
+  Mutation,
+  Arg,
+  Field,
+  ObjectType,
+} from "type-graphql";
 import "reflect-metadata";
-import { hash } from "bcryptjs";
+import { hash, compare } from "bcryptjs";
 import { User } from "./entity/User";
+
+@ObjectType()
+class LoginResponse {
+  @Field()
+  accessToken: string;
+}
 
 @Resolver()
 export class UserResolver {
   @Query(() => String)
   hello() {
     return "HI!!!";
+  }
+
+  @Query(() => [User])
+  users() {
+    return User.find();
+  }
+
+  @Mutation(() => LoginResponse)
+  async login(
+    @Arg("email") email: string,
+    @Arg("password") password: string,
+  ): Promise<LoginResponse> {
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) throw new Error("No such user exists ");
+
+    const valid = compare(password, user.password);
+
+    if (!valid) throw new Error("Could not find user");
+
+    //login successfull
+
+    return {
+      accessToken: "",
+    };
   }
 
   @Mutation(() => Boolean)
--- server/src/entity/User.ts
+++ server/src/entity/User.ts
@@ -1,10 +1,14 @@
+import { ObjectType, Field, Int } from "type-graphql";
 import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from "typeorm";
 
+@ObjectType()
 @Entity("users")
 export class User extends BaseEntity {
+  @Field(() => Int)
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Field()
   @Column()
   email: string;
 
