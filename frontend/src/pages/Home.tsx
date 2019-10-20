b1fe194000f4377fcda0d02abe6beaa8f19531cc
nzbenji <benlittle53@gmail.com>
2019-10-20 08:40:50 +0100

    added register mutation on frontend

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,26 @@
-import * as React from 'react'
+import * as React from "react";
+import { Link } from "react-router-dom";
+import { useUsersQuery } from "../generated/graphql";
 
-interface Props {
-
-}
+interface Props {}
 
 export const Home: React.FC<Props> = () => {
-        return ();
-}
+  const { data } = useUsersQuery({ fetchPolicy: "network-only" });
+
+  if (!data) return <div>Loading...</div>;
+
+  return (
+    <div>
+      <div>Users:</div>
+      <ul>
+        {data.users.map(user => {
+          return (
+            <li key={user.id}>
+              {user.email}, {user.id}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
